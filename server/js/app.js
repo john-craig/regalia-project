6639a13f5db309b5139a420197294ca35ce6d682
mguarino1 <mguarino223@gmail.com>
2019-11-14 12:33:06 -0800

    adding packages and working on form validation

--- server/js/app.js
+++ server/js/app.js
@@ -1,14 +1,37 @@
-const mysql = require('mysql');
+//Express
 const express = require('express');
+const app = express();
+
+//MySQL
+const mysql = require('mysql');
+
+//Validator
+const { check , validationResult } = require('express-validator');
+
+//bodyparser
 const bodyParser = require('body-parser');
-const router = express.Router();
-const app = express()
+
+//Form session
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 app.use(express.static('client/public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
-  }));
+}));
+
+app.use(cors());
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'positronx',
+    saveUninitialized: false,
+    resave: false,
+}));
+
 const port = 5050;
 
 // Set database connection credentials
@@ -17,6 +40,7 @@ const config = mysql.createConnection ({
     user: 'admin',
     password: 'Passw0rd',
     database: 'regalia',
+    multipleStatements: true,
 });
 
 // Create a MySQL pool
@@ -60,46 +84,70 @@ app.get('/thanks', function(req, res){
     res.sendFile('thanks.html', {root: './client'})
 });
 
-var tempData = [];
 
-app.post('/submit', function(req, res) {
+
+app.post('/submit', [
+    check('fname').not().isEmpty().withMessage("First name is required."),
+    chekc('lname').not().isEmpty().withMessage("Last name is required."),
+] , (req, res) => {
     
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).json({errors: errors.array()});
+    }
+
     var data = req.body;
 
-    /*
-    var fac_sql = "INSERT INTO faculty (FacultyID, First_Name, Last_Name, Email) VALUES ("
-        data.cwid,
-        data.name,
-        data.name,
-        data.email
-    ")";
-    var gow_sql = "INSERT INTO gowns (Height, Weight) VALUES ("
-        data.height,
-        data.weight
-    ")";
-    var cap_sql = "INSERT INTO caps (Cap_Size) VALUES ("
-
-    ")";
-    var col_sql = "INSERT INTO college(College_Name, College_City, College_State) VALUES ("
-        data.college_name,
-        data.college_city,
-        data.college_state
-    ")";
-    
-    config.query(fac_sql, function(err, result) {
+    //Faculty table
+    config.query("INSERT INTO faculty (FacultyID, First_Name, Last_Name, Email) VALUES ('" + data.cwid + "', '" + data.fname + "', '" + data.lname + "', '" + data.email + "')", function(err, res) {
         if (err) throw err;
-    });
-    */
+    })
 
-    tempData.push(data);
+    //Gowns table
+    config.query("INSERT INTO gowns (GownID, Height, Weight) VALUES ('" + data.cwid + "', '" + data.heightFeet + " " + data.heightInches  + "', '" + data.weight + "')", function(err, res) {
+        if (err) throw err;
+    })
 
-    console.log(data);
-    //res.redirect('/thanks');
+    //Caps table
+    config.query("INSERT INTO caps (CapID, Cap_Size) VALUES ('" + data.cwid + "', '" + data.cap_size + "')", function(err, res) {
+        if (err) throw err;
+    })
+
+    //Colleges table
+    config.query("INSERT INTO colleges (CollegeID, College_Name, College_City, College_State) VALUES ('" + data.cwid + "', '" + data.college_name + "', '" + data.college_city + "', '" + data.college_state + "')", function (err, res) {
+        if (err) throw err;
+    })
+
+    let date = new Date();
+    let yyyy = date.getFullYear(); 
+    let mm = ("0" + (date.getMonth() + 1)).slice(-2);
+    let dd = ("0" + date.getDate()).slice(-2);
+
+    //Order table
+    config.query("INSERT INTO orders (Date_Posted, FacultyID, GownID, CapID, CollegeID) VALUES ('" + yyyy + "-" + mm + "-" + dd + "', '" + data.cwid + "', '" + data.cwid + "', '" + data.cwid + "', '" + data.cwid + "')", function (err, res) {
+        if (err) throw err;
+    })
+
+    res.redirect('/thanks');
 
 })
 
 app.get('/api/datatable', function(req, res) {
-    res.send(tempData);
+    //res.send(tempData);
+
+    //dataRow = [];
+
+    config.query("SELECT FacultyID, First_Name, Last_Name, Email, Weight, Height, College_Name, College_City, College_State FROM faculty, gowns, colleges WHERE faculty.FacultyID = gowns.GownID", function (err, data) {       
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.send(data)
+        console.log(data[0]);
+    })
+
+
+    
 })
 
 app.post('/api/admin/add', function(req, res) {
