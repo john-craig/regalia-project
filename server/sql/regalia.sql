c1e1867bb597714f7fcb8c4bf3b12a0f29468a9e
john-craig <john.craig1@marist.edu>
2019-11-26 16:40:44 -0800

    db work

--- server/sql/regalia.sql
+++ server/sql/regalia.sql
@@ -2,18 +2,9 @@ DROP DATABASE if exists regalia;
 CREATE DATABASE regalia;
 USE regalia;
 
-CREATE TABLE faculty (
-    FacultyID int NOT NULL,
-    First_Name text,
-    Last_Name text,
-    Email text,
-    Hashed_Pass text,
-    PRIMARY KEY (FacultyID)	
-);
-
 CREATE TABLE orders (
     Date_Posted date NOT NULL,
-    FacultyID int NOT NULL,
+    ID int NOT NULL,
     Height text,
     Weight text,
     Cap_Size text,
@@ -21,15 +12,24 @@ CREATE TABLE orders (
     College_Name text NOT NULL,
     College_City text NOT NULL,
     College_State text NOT NULL,
-    PRIMARY KEY (FacultyID),
-    FOREIGN KEY (FacultyID) REFERENCES faculty(FacultyID)
+    PRIMARY KEY (ID)
 );
 
-CREATE TABLE admin (
-    AdminID int DEFAULT 0,
+CREATE TABLE users (
+    Email varchar(50) NOT NULL,    
+    First_Name text,
+    Last_Name text,
+    ID int NOT NULL,
+    Hashed_Pass text,
+    PRIMARY KEY (Email),
+    FOREIGN KEY (ID) REFERENCES orders(ID)	
+);
+
+CREATE TABLE admins (
+    AdminID int NOT NULL,
     Email text,
     PRIMARY KEY (AdminID)
-)
+);
 
 
 
