033ecf67f9aafaf1d44a7fcb77700936093c54a6
mguarino1 <mguarino223@gmail.com>
2019-11-30 09:25:45 -0800

    Register page now correctly checks for site secret

--- server/js/routes/index.js
+++ server/js/routes/index.js
@@ -43,70 +43,100 @@ router.post('/registerSubmit', (req, res) => {
     //var first = data.name.split(" ")[0];
     //var last = data.name.split(" ")[1];
     var email = data.email;
+    //Assumes that the password and password2 have already been checked against one another on the page before this call
     var pass = data.password;
 
-    //Note -- this assumes that the email will not have numeric characters before the '@' sign
+    //Note -- this assumes that the email will not have numeric characters before the '@' sign; should be okay as long as validation occurs before this call
     var first = email.split("@")[0].split(".")[0]
     var last = email.split("@")[0].split(".")[1]
 
     var secret = data.secret
 
+    var isAdmin = false;
+    var canCreate = false;
+
     config.query("SELECT Secret_Code FROM secrets ORDER BY Date_Set DESC LIMIT 1", (err, row) => {
         if(err) throw (err);
+        
+        var code = row[0].Secret_Code
 
-        console.log("Newest:")
-        console.log(row)
+        /*console.log("Normal account")
+        console.log(code)
+        console.log(secret)
+        console.log(code == secret)*/
+        
+        if(code == secret){
+            canCreate = true;
+        } else {
+            canCreate = false;
+        }
+
+        config.query("SELECT Secret_Code FROM secrets ORDER BY Date_Set ASC LIMIT 1", (err, row) => {
+            if(err) throw (err);
+    
+            var admin_code = row[0].Secret_Code
+    
+            /*console.log("Admin account")
+            console.log(admin_code)
+            console.log(secret)
+            console.log(admin_code == secret)*/
+            
+            if(admin_code == secret){
+                isAdmin = true;
+                canCreate = true;
+            } else {
+                isAdmin = false;
+            }
+
+            if(canCreate) {
+                config.query("SELECT COUNT(Email) FROM users WHERE Email =  + '" + data.email + "'", (err, row) => {
+                    if(err) throw (err);
+                    
+                    var count = row[0]['COUNT(Email)'];
+            
+                    if(count > 0) {
+                        //Redirect to registration page with error message
+                        console.log("fail");
+                        res.sendFile('register.html', {root: './client'});
+            
+                    } else {
+                        //config.query("SELECT")
+            
+                        //Create new user
+                        var newUser = User.newUser(
+                            id,
+                            first,
+                            last,
+                            email,
+                            pass,
+                            isAdmin
+                        );
+                        
+                        //Hash password
+                        bcrypt.genSalt(10, (err, salt) => 
+                            bcrypt.hash(pass, salt, (err, hash) => {
+                                if(err) throw err;
+                                //Set hased password
+                                newUser.pass = hash;
+                                //Save the user data
+                                config.query("INSERT INTO users VALUES ('" + id + "', '" + email + "', '" + first + "', '" + last + "', '" + hash + "', " + isAdmin + ")", (err, res) => {
+                                    if(err) throw err;
+                                    console.log(res)
+                                });
+                            
+                        }));
+                        res.redirect('/login'); 
+                    }
+                    
+                })
+            }
+        })
     })
 
-    config.query("SELECT Secret_Code FROM secrets ORDER BY Date_Set ASC LIMIT 1", (err, row) => {
-        if(err) throw (err);
-
-        console.log("Oldest:")
-        console.log(row)
-    })
-
-    console.log(secret)
-      
+    
     //Check for existing user
     /*
-    config.query("SELECT COUNT(Email) FROM users WHERE Email =  + '" + data.email + "'", (err, row) => {
-        if(err) throw (err);
-        
-        var count = row[0]['COUNT(Email)'];
-
-        if(count > 0) {
-            //Redirect to registration page with error message
-            console.log("fail");
-            res.sendFile('register.html', {root: './client'});
-
-        } else {
-            //config.query("SELECT")
-
-            //Create new user
-            var newUser = User.newUser(
-                id,
-                first,
-                last,
-                email,
-                pass
-            );
-            
-            //Hash password
-            bcrypt.genSalt(10, (err, salt) => 
-                bcrypt.hash(pass, salt, (err, hash) => {
-                    if(err) throw err;
-                    //Set hased password
-                    newUser.pass = hash;
-                    //Save the user data
-                    config.query("INSERT INTO users VALUES ('" + id + "', '" + email + "', '" + first + "', '" + last + "', '" + hash + "')", (err, res) => {
-                        if(err) throw err;
-                    });
-                
-            }));
-            res.redirect('/login'); 
-        }
-        
-    })
+    
     */
 });
 
