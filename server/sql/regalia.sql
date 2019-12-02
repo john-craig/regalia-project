30423ed6b37b40f2eb934e98d9c73884f7544977
mguarino1 <mguarino223@gmail.com>
2019-12-02 12:58:27 -0800

    Sync

--- server/sql/regalia.sql
+++ server/sql/regalia.sql
@@ -9,7 +9,8 @@ CREATE TABLE users (
     Last_Name text NOT NULL,
     Hashed_Pass text NOT NULL,
     IsAdmin tinyint NOT NULL,
-    PRIMARY KEY (ID)	
+    PRIMARY KEY (ID),
+    ON DELETE CASCADE
 );
 
 CREATE TABLE orders (
@@ -24,8 +25,7 @@ CREATE TABLE orders (
     College_City text NOT NULL,
     College_State text NOT NULL,
     PRIMARY KEY (OrderID),
-    FOREIGN KEY (UserID) REFERENCES users(ID),
-    ON DELETE CASCADE
+    FOREIGN KEY (UserID) REFERENCES users(ID)
 );
 
 CREATE TABLE admins (
